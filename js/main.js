4c2fa38ffe0625a9c79f3ec113a580ab2a606326
Maksim <m.yanov1998@gmail.com>
2020-06-06 16:58:19 +0700

    Added some functionality in my test console app

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
 "use strict";
 
-const filmsCount = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
+let filmsCount;
+
+function start() {
+	filmsCount = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
+
+	while (filmsCount == '' || filmsCount == null || isNaN(filmsCount)) {
+		filmsCount = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
+	}
+}
+
+function showMyDB() {
+	if (personalMoviesDB.private === false) {
+		console.log(personalMoviesDB);
+	}
+}
+
+function writeYourGanre() {
+	for (let i = 1; i < 4; i++) {
+		let ganre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+		if (ganre == '' || ganre == null) {
+			i--;
+		}
+		else {
+			personalMoviesDB.ganre.push(ganre);
+		}
+	}
+}
+
+function askAboutFilms() {
+	let i = 0;
+	while (i < 2) {
+		const a = prompt('Один из просмотренных вами фильмов?', ""),
+			b = +prompt('На сколько баллов вы его оцените?', "");
+
+		if (a != '' && a != null && a.length <= 50) {
+			personalMoviesDB.movie[a] = b;
+			i++;
+		}
+	}
+}
 
 const personalMoviesDB = {
 	count: filmsCount,
@@ -10,12 +49,17 @@ const personalMoviesDB = {
 	private: false
 };
 
-const a = prompt('Один из просмотренных вами фильмов?', ""),
-	b = +prompt('На сколько баллов вы его оцените?', ""),
-	c = prompt('Один из просмотренных вами фильмов?', ""),
-	d = +prompt('На сколько баллов вы его оцените?', "");
-
-personalMoviesDB.movie[a] = b;
-personalMoviesDB.movie[c] = d;
+if (personalMoviesDB.count < 10) {
+	alert("Просмотренно довольно мало фильмов");
+}
+else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+	alert("Вы классический зритель");
+}
+else if (personalMoviesDB.count >= 30) {
+	alert("Да вы оказывается киноман!");
+}
+else {
+	alert("Произошла ошибка!");
+}
 
 console.log(personalMoviesDB);
