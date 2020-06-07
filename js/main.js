15446598f4a1a854f1ba29d498d372544b15d430
Maksim <m.yanov1998@gmail.com>
2020-06-07 12:59:08 +0700

    Overwrite in object orientied style

--- js/main.js
+++ js/main.js
@@ -1,53 +1,67 @@
 "use strict";
 
-let filmsCount;
-
-function start() {
-	filmsCount = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
-
-	while (filmsCount == '' || filmsCount == null || isNaN(filmsCount)) {
-		filmsCount = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
-	}
-}
-
-function showMyDB() {
-	if (personalMoviesDB.private === false) {
-		console.log(personalMoviesDB);
-	}
-}
-
-function writeYourGanre() {
-	for (let i = 1; i < 4; i++) {
-		let ganre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-		if (ganre == '' || ganre == null) {
-			i--;
-		}
-		else {
-			personalMoviesDB.ganre.push(ganre);
-		}
-	}
-}
-
-function askAboutFilms() {
-	let i = 0;
-	while (i < 2) {
-		const a = prompt('Один из просмотренных вами фильмов?', ""),
-			b = +prompt('На сколько баллов вы его оцените?', "");
-
-		if (a != '' && a != null && a.length <= 50) {
-			personalMoviesDB.movie[a] = b;
-			i++;
-		}
-	}
-}
-
 const personalMoviesDB = {
-	count: filmsCount,
+	count: '',
 	movie: {},
 	actor: {},
 	ganre: [],
-	private: false
+	private: false,
+
+	start: function () {
+		this.count = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
+
+		while (this.count == '' || this.count == null || isNaN(this.count)) {
+			this.count = +prompt('Сколько фильмов вы посмотрели в этом году?', '');
+		}
+	},
+
+	showMyDB: function () {
+		if (this.private === false) {
+			console.log(personalMoviesDB);
+		}
+		else {
+			alert("Это закрытая информация!");
+		}
+	},
+
+	writeYourGanre() {
+		for (let i = 1; i < 4; i++) {
+			let ganre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+			if (ganre == '' || ganre == null) {
+				i--;
+			}
+			else {
+				this.ganre.push(ganre);
+			}
+		}
+
+		this.ganre.forEach(function(item, index) {
+			console.log(`Любимый жанр #${index+1} - это ${item}`);
+		});
+	},
+
+	askAboutFilms: function () {
+		let i = 0;
+		while (i < 2) {
+			const a = prompt('Один из просмотренных вами фильмов?', ""),
+				b = +prompt('На сколько баллов вы его оцените?', "");
+
+			if (a != '' && a != null && a.length <= 50) {
+				this.movie[a] = b;
+				i++;
+			}
+		}
+	},
+
+	toggleVisible: function () {
+		this.private = !this.private;
+	}
 };
+
+// personalMoviesDB.start();
+// personalMoviesDB.writeYourGanre();
+personalMoviesDB.toggleVisible();
+personalMoviesDB.showMyDB();
 
 if (personalMoviesDB.count < 10) {
 	alert("Просмотренно довольно мало фильмов");
@@ -61,5 +75,3 @@ else if (personalMoviesDB.count >= 30) {
 else {
 	alert("Произошла ошибка!");
 }
-
-console.log(personalMoviesDB);
